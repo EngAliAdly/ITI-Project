743cbfe6e99e4d911eba5d7d400fbcf3e19f0a6d
peter mamdouh <33961635+peterM65@users.noreply.github.com>
2023-11-06 22:28:21 +0200

    V1

--- ClinicMaster.Web/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace ClinicMaster.Web.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
