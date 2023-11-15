6af56c34ecd449c9cf60de6068ce79c346ddf038
peter mamdouh <33961635+peterM65@users.noreply.github.com>
2023-11-16 01:30:04 +0200

    .

--- ClinicMaster.Web/Program.cs
+++ ClinicMaster.Web/Program.cs
@@ -75,7 +75,7 @@ namespace ClinicMaster.Web
 
             app.MapControllerRoute(
                 name: "default",
-                pattern: "{controller=Home}/{action=Index}/{id?}");
+                pattern: "{controller=Account}/{action=Login}/{id?}");
 
             using var scope = app.Services.CreateScope();
             var services = scope.ServiceProvider;
